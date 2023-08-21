102fd3d480864984be2332f7697f219607014d7b
Paulo Filipe <paulo.vasconcelos@afinz.com.br>
2023-08-21 04:18:30 -0300

    test(user) update user tests

--- src/__test__/user.test.js
+++ src/__test__/user.test.js
@@ -1,27 +1,47 @@
 import request from 'supertest';
 import { expect } from 'chai';
-import { app } from '../../server.mjs';
+import createRandomName from './helpers/randomName.mjs';
+import userMutation from './helpers/user-create-mutation.mjs';
+import updateMutation from './helpers/user-update-mutation.mjs';
+import app from './helpers/app-url.mjs';
 
-describe('GraphQL API', () => {
+
+describe('GraphQL API - User', () => {
   it('should create a user via the createUser mutation', async () => {
-    const mutation = `
-      mutation {
-        createUser(name: "Test User", email: "test@example.com") {
-          id
-          name
-          email
-        }
-      }
-    `;
+    const mockName = createRandomName(10);
 
     const response = await request(app)
-      .post('/graphql') 
-      .send({ query: mutation })
+      .post('/graphql')
+      .send({ query: userMutation(mockName) })
       .set('Content-Type', 'application/json');
 
     const { data } = response.body;
     expect(data.createUser).to.have.property('id').that.is.a('string');
-    expect(data.createUser.name).to.equal('Test User');
-    expect(data.createUser.email).to.equal('test@example.com');
+    expect(data.createUser.name).to.equal(mockName);
+    expect(data.createUser.email).to.equal(`${mockName}@example.com`);
+  });
+
+  it('should edit a user via the updateUser mutation', async () => {
+    const mockUserName = createRandomName(10);
+
+    const createResponse = await request(app)
+      .post('/graphql')
+      .send({ query: userMutation(mockUserName) })
+      .set('Content-Type', 'application/json');
+
+    const { data: createData } = createResponse.body;
+    const id = createData.createUser.id;
+
+    expect(createData.createUser.email).to.equal(`${mockUserName}@example.com`);
+
+    const mockedNewEmail = createRandomName(5);
+
+    const updateResponse = await request(app)
+      .post('/graphql')
+      .send({ query: updateMutation(id, `${mockedNewEmail}@test.com`) })
+      .set('Content-Type', 'application/json');
+
+    const { data: updatedData } = updateResponse.body;
+    expect(updatedData.updateUser.email).to.equal(`${mockedNewEmail}@test.com`);
   });
 });
